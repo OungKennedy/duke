df120753b7101f95b8666937f28f263497cd9470
OungKennedy <oungkennedy@gmail.com>
2019-08-20 00:08:04 +0800

    did tutorial part 1. excercises incomplete.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,9 @@
-public class Duke {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+public class Duke extends Application {
     /** this is the main function for duke. */
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -8,4 +13,17 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
     }
+
+    @Override
+    public void start(Stage stage) {
+        Label helloWorld = new Label("Hello World!"); // Creating a new Label control
+        Scene scene = new Scene(helloWorld); // Setting the scene to be our Label
+        stage.setScene(scene); //Setting the stage to show our screen
+        stage.show(); //Render the stage
+
+
+
+    }
 }
+
+
