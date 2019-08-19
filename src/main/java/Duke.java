bdcfa79c4f1c459110a49e2154e3c9dc56d71b33
OungKennedy <oungkennedy@gmail.com>
2019-08-19 23:15:18 +0800

    set up gradle.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,5 @@
 public class Duke {
+    /** this is the main function for duke. */
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
